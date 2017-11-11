49c2407c68af18b67e2e3f1421cca94416e6207f
Eric Cook <office@bettersearchllc.com>
2017-11-11 18:51:04 -0500

    Initial Commit

---
+++ MachineLearning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MachineLearning
+//
+//  Created by Eric Cook on 11/11/17.
+//  Copyright © 2017 Better Search, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
